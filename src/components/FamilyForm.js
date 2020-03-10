ff492238f1ed463fa5c6823ad41380f73b5f6f2e
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-03-10 10:14:05 -0400

    Add Styling to create family form
    Add bootstrap styling and classes to create family form
    
    Close #13

--- src/components/FamilyForm.js
+++ src/components/FamilyForm.js
@@ -1,61 +1,80 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 const FamilyForm = ({ family, handleSubmit, handleChange, cancelPath }) => (
-  <form onSubmit={handleSubmit}>
-    <label>Last Name</label>
-    <input
-      placeholder="Doe"
-      value={family.familyName}
-      name="family Name"
-      onChange={handleChange}
-    />
+  <div className='row'>
+    <div className="col-sm-10 cold-md-8 mx-auto mt-5">
+      <h3> Create a New Family </h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='last name'>
+          <Form.Label>Last Name</Form.Label>
+          <input
+            placeholder="Doe"
+            value={family.familyName}
+            name="family Name"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-    <label>Parent One Name</label>
-    <input
-      placeholder="John"
-      value={family.parentOneName}
-      name="parent One Name"
-      onChange={handleChange}
-    />
+        <Form.Group controlId='parent one name'>
+          <Form.Label>Parent One Name</Form.Label>
+          <input
+            placeholder="John"
+            value={family.parentOneName}
+            name="parentOneName"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId='parent two name'>
+          <Form.Label>Parent Two Name</Form.Label>
+          <input
+            placeholder="Jane"
+            value={family.parentTwoName}
+            name="parentTwoName"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-    <label>Parent Two Name</label>
-    <input
-      placeholder="Jane"
-      value={family.parentTwoName}
-      name="parent Two Name"
-      onChange={handleChange}
-    />
+        <Form.Group controlId='Number of kids'>
+          <Form.Label>Number of Kids</Form.Label>
+          <input
+            placeholder="3"
+            value={family.numberOfKids}
+            name="numberOfKids"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-    <label>Number of Kids</label>
-    <input
-      placeholder="3"
-      value={family.numberOfKids}
-      name="number of kids"
-      onChange={handleChange}
-    />
+        <Form.Group controlId='city'>
+          <Form.Label>City</Form.Label>
+          <input
+            placeholder="Boston"
+            value={family.city}
+            name="city"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-    <label>City</label>
-    <input
-      placeholder="Boston"
-      value={family.city}
-      name="city"
-      onChange={handleChange}
-    />
+        <Form.Group controlId='state'>
+          <Form.Label>State</Form.Label>
+          <input
+            placeholder="Massachusetts"
+            value={family.state}
+            name="state"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-    <label>State</label>
-    <input
-      placeholder="Massachusetts"
-      value={family.state}
-      name="state"
-      onChange={handleChange}
-    />
-
-    <button type="submit">Submit</button>
-    <Link to={cancelPath}>
-      <button>Cancel</button>
-    </Link>
-  </form>
+        <Button varitent="primary" type="submit">Submit</Button>
+        <Link to={cancelPath}>
+          <Button>Cancel</Button>
+        </Link>
+      </Form>
+    </div>
+  </div>
 )
 
 export default FamilyForm
